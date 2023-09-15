2249aadfab48495dbb20c67e3af2f0c2bd089bdd
tysonpbr <thetysonbrown@gmail.com>
2023-09-14 18:03:39 -0700

    started login and server info

---
+++ client/app/src/main/java/com/tysonpbr/cpen_321_m1/LoginServerActivity.java
@@ -0,0 +1,14 @@
+package com.tysonpbr.cpen_321_m1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginServerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_server);
+    }
+}
