cfb1f17f6d4cc6e5818e6f38a9e356e16973d974
tysonpbr <thetysonbrown@gmail.com>
2023-09-14 20:08:42 -0700

    fixed package.json

--- server/server.js
+++ server/server.js
@@ -2,13 +2,25 @@ var express = require('express');
 var app = express();
 
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://127.0.0.1:27017";
+const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
 app.use(express.json());
 
 app.get('/', (req,res) => {
   res.send("Hello World");
+});
+
+app.get('/ipAddress', (req,res) => {
+  res.send("Hello World");
+});
+
+app.get('/localTime', (req,res) => {
+  res.send("Hello World");
+});
+
+app.get('/name', (req,res) => {
+  res.send("Tyson Brown");
 });
 
 var server = app.listen(8081, (req,res) => {
